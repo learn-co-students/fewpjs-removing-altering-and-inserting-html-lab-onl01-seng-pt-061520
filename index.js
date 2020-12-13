5d6112f7b4639f5d19443bbbbc8cd3005bea5df9
mjaber <abujaber44@gmail.com>
2020-12-13 12:56:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+main.remove("main");
+
+
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+let node = document.createTextNode('Moe is the champion');
+newHeader.appendChild(node);
