bce9cd569e0779a0edb0a5d6b07ef29a246cd675
nickd <nickdiaz58@gmail.com>
2020-12-17 08:18:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main = document.getElementById('main');
+main.remove();
+
+var newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Your Mom is the champion";
