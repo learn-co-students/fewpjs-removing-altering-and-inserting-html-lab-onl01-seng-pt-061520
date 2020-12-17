98b25b342698db6cb2dea2e47338d819aae787bd
Bryce B <bryce.boddy@gmail.com>
2020-12-17 15:41:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let main = document.getElementById("main");
 main.remove();
 
-let newHeader = document.createElement('H1#victory');
-newHeader.innerHTML = "YOUR-NAME"
+let newHeader = document.createElement('H1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "YOUR-NAME is the champion"
