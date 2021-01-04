d367ce0c7dcc01ddeca02cf3ee4e9567d8706ab1
jpv2018 <jason.viola@mavs.uta.edu>
2021-01-04 12:11:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove('main');
+
+var newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = '<h1>Jason is the champion</h1>';
+newHeader.append("victory");
