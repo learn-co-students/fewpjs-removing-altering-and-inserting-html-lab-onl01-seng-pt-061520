489ae5b607554349fc0e551fd3bd2b8ac8ff12e2
Teodoro Garcia <tedtke514@gmail.com>
2021-01-09 20:22:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Ted is the champion!";
+newHeader.className = "victory";
+
+// let newHeader =  document.querySelector(h1#victory)
+// element.innerHTML = 'YOUR-NAME is the champion'
